c2fcf1ec93e326207630406cb6e46faf445e5ba1
jsc9jy <31700320+jsc9jy@users.noreply.github.com>
2021-08-15 18:51:36 -0400

    ItemController Impl
    Item Controller implementation + formatting

---
+++ WishList/Controllers/ItemController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WishList.Data;
+using WishList.Models;
+
+namespace WishList.Controllers
+{
+    public class ItemController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ItemController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View("Index", _context.Items);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View("Create");
+        }
+
+        [HttpPost]
+        public IActionResult Create(Item item)
+        {
+            _context.Add(item);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int Id)
+        {
+            Item itemToDelete = _context.Items.FirstOrDefault(item => item.Id == Id);
+            _context.Items.Remove(itemToDelete);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
